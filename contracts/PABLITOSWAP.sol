a8dd63626e2ab9006dd95348547f9eb59840ffe5
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-03-14 04:35:49 +0100

    Add modifier with comment and comment for constructor

--- contracts/PABLITOSWAP.sol
+++ contracts/PABLITOSWAP.sol
@@ -10,9 +10,19 @@ contract PABLITOSWAP {
 
     address public immutable tokenA;
     address public immutable tokenB;
+    address public owner;
 
+// these variables: are set once in deployment and can't be changed
     constructor (address _tokenA, address _tokenB) {
         tokenA = _tokenA;
         tokenB = _tokenB;
+        owner = msg.sender;
     }
+
+// access control: only owner can starts functions below modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+    
 }
