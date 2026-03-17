eedb8237fc149c29c42b5a4bf2d0a9f511345fb4
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-03-17 08:21:38 +0100

    Replace from basic transfers to SafeERC20 transfers (main contract)

--- contracts/PABLITOSWAP.sol
+++ contracts/PABLITOSWAP.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 using SafeERC20 for IERC20;
 
 
+
 contract PABLITOSWAP {
 
     address public immutable tokenA;
@@ -29,20 +30,24 @@ contract PABLITOSWAP {
         _;
     }
         
+
     function swap (address _tokenA, address _tokenB, uint256 amount) public { 
         require(amount > 0, "Amount must be > 0");                                      // Remember: change from require/revert to custom errors with "if" (after tests / optimalization gas)
-    
+
+        uint256 public amountIn
+        uint256 public amountOut
 
         // Check tokens: what address user used = what address I implemented in code
         require(_tokenA == tokenA || _tokenA == tokenB, "Unsupported tokenA");                      
         require(_tokenB == tokenA || _tokenB == tokenB, "Unsupported tokenB");
 
         // Token transfers from user to my contract address
-        IERC20(tokenA).transferFrom(msg.sender, address(this), amount);
-        IERC20(tokenB).transferFrom(msg.sender, address(this), amount);
+        IERC20(tokenA).safeTransferFrom(msg.sender, address(this), amount);
+        IERC20(tokenB).safeTransferFrom(msg.sender, address(this), amount);
+
+
+
 
     }   
-
-
 
 }
