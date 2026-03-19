b5cb3ba41ba678e4b5c53d68a97724fce2ec19a6
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-03-19 06:15:15 +0100

    Add constructor and variable for call (LP contract)

--- contracts/PABLITOSWAPLP.sol
+++ contracts/PABLITOSWAPLP.sol
@@ -13,6 +13,13 @@ contract PABLITOSWAPLP {
     uint256 public reserveA;                    // WETH
     uint256 public reserveB;                    // USDC
     uint256 public fee = 3;                     // 0.3%
+    address public pablitoSwap;                 // main contract
 
 
+    // these variables: are set once in deployment and can't be changed
+    constructor (address _tokenA, address _tokenB) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+        pablitoSwap = msg.sender;
+    }
 }
