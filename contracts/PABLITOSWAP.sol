de342ecdcdf614ab07b3e283a2f0ce398c53270a
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-03-23 09:23:13 +0100

    Remove function swap (main SWAP contract)

--- contracts/PABLITOSWAP.sol
+++ contracts/PABLITOSWAP.sol
@@ -31,21 +31,8 @@ contract PABLITOSWAP {
     }
         
 
-    function swap (address _tokenA, address _tokenB, uint256 amount) public { 
-        require(amount > 0, "Amount must be > 0");                                      // Remember: change from require/revert to custom errors with "if" (after tests / optimalization gas)
-
-        uint256 public amountIn
-        uint256 public amountOut
-
-        // Check tokens: what address user used = what address I implemented in code
-        require(_tokenA == tokenA || _tokenA == tokenB, "Unsupported tokenA");                      
-        require(_tokenB == tokenA || _tokenB == tokenB, "Unsupported tokenB");
-
-        // Token transfers from user to my contract address
-        IERC20(tokenA).safeTransferFrom(msg.sender, address(this), amount);
-        IERC20(tokenB).safeTransferFrom(msg.sender, address(this), amount);
 
 
-    }   
+
 
 }
