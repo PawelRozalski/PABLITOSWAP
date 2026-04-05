922029fbe16823546f6fd645c77a97fb8eaf4dab
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-04-05 07:33:10 +0200

    Change the type and structure of variables in contract (LP)

--- contracts/PABLITOSWAPLP.sol
+++ contracts/PABLITOSWAPLP.sol
@@ -11,19 +11,20 @@ contract PABLITOSWAPLP {
 
     using SafeERC20 for IERC20;
 
-    address public tokenA;
-    address public tokenB;
+    address public immutable tokenA;
+    address public immutable tokenB;
+    address public immutable pablitoSwap;       // main contract
     uint256 public totalLiquidity;
     uint256 public reserveA;                    // WETH
     uint256 public reserveB;                    // USDC
     uint256 public fee = 3;                     // 0.3%
-    address public pablitoSwap;                 // main contract
 
 
     // these variables: are set once in deployment and can't be changed
     constructor (address _tokenA, address _tokenB) {
         tokenA = _tokenA;
         tokenB = _tokenB;
+
         pablitoSwap = msg.sender;
     }
 
