95240a1b7ca0cc78086afe18d1f26873b4ae94ed
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-05-02 05:45:04 +0200

    Rename form PABLITOSWAP to PablitoSwap (main and LP contract)

--- contracts/PABLITOSWAP.sol
+++ contracts/PABLITOSWAP.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./PABLITOSWAPLP.sol";
+import "./PablitoSwapLP.sol";
 
 
-contract PABLITOSWAP {
+contract PablitoSwap {
 
     using SafeERC20 for IERC20;
 
@@ -22,7 +22,7 @@ contract PABLITOSWAP {
         tokenA = _tokenA;
         tokenB = _tokenB;
         owner = msg.sender;
-        lpContract = PABLITOSWAPLP(_lpAddress);
+        lpContract = PablitoSwapLP(_lpAddress);
     }
 
 
--- contracts/PABLITOSWAPLP.sol
+++ contracts/PABLITOSWAPLP.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 
-contract PABLITOSWAPLP {
+contract PablitoSwapLP {
 
     using SafeERC20 for IERC20;
 
