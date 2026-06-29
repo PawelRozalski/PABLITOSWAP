13a41eb8d8a6db4e1021139fe351eda31f3fa361
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-06-29 09:01:06 +0200

    Add constructor (Mock)

--- src/MockERC20.sol
+++ src/MockERC20.sol
@@ -4,3 +4,11 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
+contract MockERC20 is ERC20 {
+
+    // create token ERC20 with Mock name and MOCK symbol:
+    constructor() ERC20("Mock", "MOCK") {}
+
+
+}
