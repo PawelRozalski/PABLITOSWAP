6c85e31bc90f5fbc0f2a87ccd271c5eea96427dd
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-06-29 08:04:02 +0200

    Add import src for create fake tokens (LP T)

--- test/PablitoSwapLP.t.sol
+++ test/PablitoSwapLP.t.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.34;
 
 // import test library from Foundry
 import "forge-std/Test.sol";
-import "../src/PablitoSwapLP.sol"; 
+
+import "../src/PablitoSwapLP.sol";
+import "../src/MockERC20.sol";
 
 
 contract PablitoSwapLPTest is Test {
