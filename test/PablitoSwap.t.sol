831fd4774192d3ee95f616da20f62b0c4596cd26
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-08-06 07:52:46 +0200

    Add variables in general contract test (main T)

--- test/PablitoSwap.t.sol
+++ test/PablitoSwap.t.sol
@@ -11,3 +11,15 @@ import "../src/MockERC20.sol";
 
 contract PablitoSwapTest is Test {
 
+    // contract like a variable for calculate 
+    PablitoSwap public caller;
+
+        uint256 amountA = 1e18;                         // ETH
+        uint256 amountB = 1500e6;                       // USDC
+
+        MockERC20 public tokenA;        // fake token A
+        MockERC20 public tokenB;        // fake token B
+
+
+
+}
