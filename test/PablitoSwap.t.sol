22b26cece1101da6957ebe176efc55ffe6e43d5a
Paweł Różalski <ppawel.rozalski@gmail.com>
2026-08-06 07:53:56 +0200

    Add setup function (main T)

--- test/PablitoSwap.t.sol
+++ test/PablitoSwap.t.sol
@@ -20,6 +20,27 @@ contract PablitoSwapTest is Test {
         MockERC20 public tokenA;        // fake token A
         MockERC20 public tokenB;        // fake token B
 
+    // SETUP: deploy  
+    function setUp() public {
+
+        tokenA = new MockERC20();       // create fake token A
+        tokenB = new MockERC20();       // create fake token B
+
+        MockERC20(tokenA).mint(address(this), 1e18);        // new token mint for 1 value with e18 for ETH simulation
+        MockERC20(tokenB).mint(address(this), 1500e6);      // new token mint for 1500 value with e6 for USDC simulation
+
+        caller = new PablitoSwap(address(tokenA), address(tokenB));                      // add data from constructor for tests
+
+        // taked tokens:
+        deal(address(tokenA), address(this), amountA);
+        deal(address(tokenB), address(this), amountB);
+
+        // approval tokens:
+        IERC20(tokenA).approve(address(caller), amountA);
+        IERC20(tokenB).approve(address(caller), amountB);
+
+    }
+
 
 
 }
